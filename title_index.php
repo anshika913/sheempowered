8aa4d6c1484b4e42f54ee97e430b502131aa13d6
anshika913 <124578786+anshika913@users.noreply.github.com>
2023-10-02 19:56:20 +0530

    Add files via upload

---
+++ title_index.php
@@ -0,0 +1,37 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						<img class="index_logo" src="admin/images/Logo1.jpg">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+							
+							<h3>
+
+							<p>She Empowered</p>
+						
+							</h3>		
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+								<p>All in One </p>
+												<p>One Platform for the Women’s Education , Career and other growths. </p>
+								</div>		
+						</div>		
+				</div>
